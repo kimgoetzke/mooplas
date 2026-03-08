e30a5e5b989f2025aa3443f323c286b09810ec90
Kim Goetzke <kbgoetzke@gmail.com>
2026-03-08 10:44:19 +0000

    Add basic Renet tests

--- mooplas_networking_renet/src/renet/messages.rs
+++ mooplas_networking_renet/src/renet/messages.rs
@@ -30,13 +30,47 @@ pub struct ClientHandshakeOutcomeMessage {
 fn receive_netcode_transport_error_event(error_event: On<NetcodeErrorEvent>, mut commands: Commands) {
   let netcode_transport_error = &(**error_event);
   info!("Netcode transport error occurred: [{}]...", netcode_transport_error);
-  let error = match netcode_transport_error {
+  let error = map_netcode_transport_error_to_network_error(netcode_transport_error);
+  commands.trigger(error);
+}
+
+fn map_netcode_transport_error_to_network_error(netcode_transport_error: &NetcodeTransportError) -> NetworkErrorEvent {
+  match netcode_transport_error {
     NetcodeTransportError::Renet(e) => NetworkErrorEvent::RenetDisconnect(e.to_string()),
     NetcodeTransportError::Netcode(e) => match e {
       NetcodeError::Disconnected(reason) => NetworkErrorEvent::NetcodeDisconnect(reason.to_string()),
       _ => NetworkErrorEvent::NetcodeTransportError(e.to_string()),
     },
     NetcodeTransportError::IO(e) => NetworkErrorEvent::IoError(e.to_string()),
-  };
-  commands.trigger(error);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use bevy_renet::netcode::{NetcodeError, NetcodeTransportError};
+  use mooplas_networking::prelude::NetworkErrorEvent;
+
+  #[test]
+  fn map_netcode_transport_error_to_network_error_maps_netcode_disconnected_to_netcode_disconnect_event() {
+    use bevy_renet::netcode::NetcodeDisconnectReason;
+    let net_err = NetcodeTransportError::Netcode(NetcodeError::Disconnected(
+      NetcodeDisconnectReason::DisconnectedByServer,
+    ));
+    let res = map_netcode_transport_error_to_network_error(&net_err);
+    match res {
+      NetworkErrorEvent::NetcodeDisconnect(s) => assert!(s.contains("server") || s.contains("terminated")),
+      _ => panic!("Expected NetworkErrorEvent::NetcodeDisconnect"),
+    }
+  }
+
+  #[test]
+  fn map_netcode_transport_error_to_network_error_maps_io_error_to_io_error_event() {
+    let io_err = NetcodeTransportError::IO(std::io::Error::new(std::io::ErrorKind::Other, "io"));
+    let res = map_netcode_transport_error_to_network_error(&io_err);
+    match res {
+      NetworkErrorEvent::IoError(s) => assert!(s.contains("io")),
+      _ => panic!("Expected NetworkErrorEvent::IoError"),
+    }
+  }
 }
